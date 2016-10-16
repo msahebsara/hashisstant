9cde8d3c6dcc57b8d9719d710c546865ece80dad
Matt Sahebsara <msahebsara@live.ca>
2016-10-16 12:57:07 -0600

    Update readme

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require 'HashHelper.php';
 
 use App\HashHelper;
 
-$hash = HashHelper::create('password', 12); 
+$hash = HashHelper::create('password'); 
 
 var_dump(HashHelper::check('notpass', $hash)); 
 var_dump(HashHelper::check('password', $hash));
